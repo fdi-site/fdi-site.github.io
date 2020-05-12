a843d2ee53d2a72fe6921d5346217da36a9bac59
fdi-site <65225280+fdi-site@users.noreply.github.com>
2020-05-12 23:45:41 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <div id="siteData">
+        0adfs
+    </div>
+</body>
+</html>
